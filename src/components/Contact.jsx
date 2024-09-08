ac0645d7c695511b6e13d7703379f38a70e0dca0
rahul8073 <rahulkumarsingh1473@gmail.com>
2024-09-09 00:58:04 +0530

    fix

---
+++ src/components/Contact.jsx
@@ -0,0 +1,127 @@
+import React from "react"
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
+import { motion } from "framer-motion"
+import Reveal from "./Reveal"
+
+const Contact = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the page from reloading
+    const formData = new FormData(e.target);
+    const data = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      message: formData.get("message"),
+    };
+    // console.log(data,"ggggggggggggg");
+    
+
+    try {
+      const response = await fetch("http://localhost:5000/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        alert("Message sent successfully!");
+        e.target.reset(); // Clear the form
+      } else {
+        alert("Failed to send message.");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      alert("Error sending message.");
+    }
+  };
+  return (
+    <div className="px-6 max-w-[1000px] mx-auto md:my-12" id="contact">
+        <Reveal>
+        <div className="grid md:grid-cols-2 place-items-center">
+            <div>
+                <div className="text-gray-300 my-3">
+                    <h3 className="text-4xl font-semibold mb-5">About <span>Me</span></h3>
+                    <p className="text-justify leading-7 w-11/12 mx-auto">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                        eos, quam vel quisquam, explicabo sit labore dignissimos optio
+                        ratione quibusdam doloribus pariatur consequuntur sint.
+                        Reprehenderit cupiditate possimus facere quasi voluptatem?
+                    </p>
+                </div>
+
+                <div className="flex mt-10 items-center gap-7">
+                    <div className="bg-gray-800/40 p-4 rounded-lg">
+                        <h3 className="md:text-4xl text-2xl font-semibold text-white">4
+                            <span>+</span>
+                        </h3>
+                        <p className="text-xs md:text-base"><span>Projects</span></p>
+                    </div>
+
+                    <div className="bg-gray-800/40 p-5 rounded-lg">
+                        <h3 className="md:text-4xl text-2xl font-semibold text-white">1
+                            <span>+</span>
+                        </h3>
+                        <p className="text-xs md:text-base"><span>years of experience</span></p>
+                    </div>
+
+                    <div className="bg-gray-800/40 p-5 rounded-lg">
+                        <h3 className="md:text-4xl text-2xl font-semibold text-white">5
+                            <span>+</span>
+                        </h3>
+                        <p className="text-xs md:text-base"><span>happy clients</span></p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <form
+                onSubmit={handleSubmit} 
+                method="POST"
+                className=" max-w-6xl p-5 md:p-12"
+                id="form"
+            >
+            <p className="text-gray-100 font-bold text-xl mb-2">
+              Let´s connect!
+            </p>
+            <input
+                type="text"
+                id="name"
+                placeholder="Your Name ..."
+                name="name"
+                className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
+            />
+            <input
+              type="email"
+              id="email"
+              placeholder="Your Email ..."
+              name="email"
+              className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
+            />
+            <textarea
+              name="message"
+              id="textarea"
+              cols="30"
+              rows="4"
+              placeholder="Your Message ..."
+              className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
+            />
+            <button
+              type="submit"
+              className="w-full py-3 rounded-md text-gray-100 font-semibold text-xl bg-primary-color"
+            >
+              Send Message
+            </button>
+            
+          </form>
+
+        </div>
+        
+        </Reveal>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Hero.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import profilepic from "../assets/profpic.png";
+import { TypeAnimation } from "react-type-animation";
+import ShinyEffect from "./ShinyEffect";
+import resume from'../assets/RahulSingh.pdf'
+import {
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineLink,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import {
+  DiCss3,
+  DiHtml5,
+  DiJavascript1,
+  DiNodejsSmall,
+  DiReact,
+} from "react-icons/di";
+import { motion } from "framer-motion";
+
+
+const Hero = () => {
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = resume; // Replace with the dynamic URL if needed
+        link.setAttribute('download', 'RahulSingh.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+  return (
+    <div className="mt-24 max-w-[1200px] mx-auto relative">
+        <div className="grid md:grid-cols-2 place-items-center gap-8">
+            <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            >
+                <TypeAnimation
+                    sequence={[
+                        "Mernstack Dev",
+                        1000,
+                        "React Developer",
+                        1000,
+                        "React-Native Dev",
+                        1000
+                    ]}
+                    speed={50}
+                    repeat={Infinity}
+                    className="font-bold text-gray-400 text-xl md:text-5xl italic- mb-4"
+                />
+
+                <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-gray-200 md:text-7xl text-5xl tracking-tight mb-4"
+                >
+                    HEY, I AM <br/>
+                    <span className="text-purple-500">RAHUL SINGH</span>
+                </motion.p>
+
+                <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 1 }}
+                className="text-gray-300 max-w-[300px] md:max-w-[500px] md:text-2xl text-lg mb-6"
+                >
+                    I am a passionate Mernstack developer with over 1 years of experience.
+                </motion.p>
+
+                <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 1.5 }}
+                className="flex flex-row items-center gap-6 my-4 md:mb-0"
+                >
+                    <motion.button onClick={handleDownload} whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)" }}
+                        className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
+                                     border-purple-400 rounded-xl"     
+                    >
+                  Download CV
+                        
+                    </motion.button>
+
+                    <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
+                        <motion.a  whileHover={{ scale: 1.2 }} href="https://github.com/rahul8073">
+                            <AiOutlineGithub/>
+                        </motion.a>
+
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/rahul-singh-781b5b243?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+                            <AiOutlineLinkedin/>
+                        </motion.a>
+
+                        <motion.a whileHover={{ scale: 1.2 }} href="#">
+                            <AiOutlineInstagram/>
+                        </motion.a>
+                    </div>
+                </motion.div>
+            </motion.div>
+
+
+            <motion.img 
+                src={profilepic}
+                className="w-[300px] md:w-[450px]"
+                initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1 }}
+            />
+        </div>
+
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 2 }}
+            className="flex flex-row text-7xl px-12 md:px-0 w-full justify-center items-center py-24"
+        >
+            <p className="text-gray-200 mr-6">My Tech Stack</p>
+            <DiHtml5 className="text-orange-600 mx-2" />
+            <DiCss3 className="text-blue-600 mx-2" />
+            <DiJavascript1 className="text-yellow-500 mx-2" />
+            <DiReact className="text-blue-500 mx-2" />
+            <DiNodejsSmall className="text-green-500 mx-2" />
+
+        </motion.div>
+
+        <div className="absolute inset-0 hidden md:block">
+            <ShinyEffect left={0} top={0} size={1400} />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Hero
